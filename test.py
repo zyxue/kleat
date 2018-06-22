0a287071b12214e7c6e503abd59f94034d8a7ca4
Zhuyi Xue <alfred532008@gmail.com>
2018-06-21 17:40:31 -0700

    added test_bridge_reads

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 import pysam
 
-from kleat3 import calc_ref_clv, calc_tail_length, calc_num_tail_reads
+from kleat3 import (
+    calc_strand,
+    calc_ref_clv,
+    calc_tail_length,
+    calc_num_tail_reads,
+    is_forward_tseg,
+    calc_ref_clv_from_r2c_alignment,
+)
 
 
 C2G_BAM_FILE = '../kleat3-test-data/tasrkleat-results/align_contigs2genome/cba.sorted.bam'
@@ -18,11 +25,14 @@ def test_tail_contigs():
         ]:
             continue
 
+        strand = calc_strand(contig)
         ref_clv = calc_ref_clv(contig)
         tail_length = calc_tail_length(contig)
         num_tail_reads, contig_clv = calc_num_tail_reads(contig, R2C_BAM)
 
+        # ERBB2
         if contig.query_name == "A0.R100820":
+            assert strand == '+'
             assert contig.is_reverse is True
             assert contig.reference_name == "chr17"
             assert ref_clv == 37884912
@@ -30,7 +40,9 @@ def test_tail_contigs():
             assert num_tail_reads == 2
             assert contig_clv == 4668
 
+        # BAP1
         elif contig.query_name == "A1.R26141":
+            assert strand == '-'
             assert contig.is_reverse is False
             assert contig.reference_name == "chr3"
             assert ref_clv == 52435023
@@ -40,4 +52,50 @@ def test_tail_contigs():
 
 
 def test_bridge_reads():
-    pass
+    for k, contig in enumerate(C2G_BAM):
+        if contig.query_name not in [
+                "A0.R100710",
+                "A1.S26245"
+        ]:
+            continue
+
+        strand = calc_strand(contig)
+        for read in R2C_BAM.fetch(
+                contig.query_name, 0, contig.query_length):
+            if read.is_unmapped or read.is_reverse:
+                # still possible a read is unmapped even though fetching
+                # used a specific contig, e.g.
+
+# could be an example for link reads
+# SN7001282:314:h15b0adxx:1:2206:17178:42842      73      A0.S68856       1       60      75M     =       1       0       GGGGGACAGATCTTCAGTTCTCATGACCACAAAAGAGGATACTAAAGCTCAGACAGGAGAAGAGACGTGGCCAGC     CCCFFFFFHHHHHJJJJHIIIJJIJJJIJIJJJIGIIIICGIJJJJGHHGHBHCHIEHEHIEHCHEHABBEEECC     NM:i:0  MD:Z:75 AS:i:75 XS:i:0
+# SN7001282:314:h15b0adxx:1:2206:17178:42842      133     A0.S68856       1       0       *       =       1       0       TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT     +1:A:?)8<CFDHA@/4337=:BBBB6630&07670:7317@B@B@5@88889<<<@57<B7@BBBBB;;>>>5<     AS:i:0  XS:i:0
+                continue
+
+            if not is_forward_tseg(read):
+                continue
+
+            ref_clv = calc_ref_clv_from_r2c_alignment(contig, read)
+
+            # PTEN
+            if contig.query_name == "A0.R100710":
+                assert strand == '+'
+                assert contig.reference_name == "chr10"
+
+                # there are two clvs
+                if read.query_name in [
+                        'SN7001282:314:h15b0adxx:1:2203:2771:88598',
+                        'SN7001282:314:h15b0adxx:2:1105:18463:70356'
+                ]:
+                    assert ref_clv == 89725516
+                else:
+                    assert ref_clv == 89725287
+
+            # KRAS
+            if contig.query_name == "A1.S26245":
+                assert strand == '-'
+                assert contig.reference_name == "chr12"
+                assert ref_clv == 25362769
+
+            # potential test case for bridge read
+            # PTEN	ENST00000371953	+	yes	A0.R100710	chr10	89725287, ref_clv: 89725516, 89725287
+            # KRAS	ENST00000256078	-	yes	A1.S26245	chr12	25362769, ref_clv: 25362769
