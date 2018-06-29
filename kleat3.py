7ea61a0c22bf0e1c0142d5af93972822d29eee9f
Zhuyi Xue <alfred532008@gmail.com>
2018-06-29 16:45:42 -0700

    renamed tail to suffix; factor out suffix_utils
    tail will always refer to the polyA tail of a suffix contig/reads or
    bridge/reads

--- kleat3.py
+++ kleat3.py
@@ -1,128 +1,11 @@
 import csv
 import pysam
 
+from suffix_utils import is_suffix_contig, analyze_suffix_contig
+
 
 # https://pysam.readthedocs.io/en/latest/api.html?highlight=AlignmentSegment#pysam.AlignedSegment.cigartuples
 BAM_CMATCH = 0
-BAM_CSOFT_CLIP = 4              # used to identify soft clip 
-
-
-def calc_strand(contig):
-    """
-    calculate the strand of clv (hence the corresponding gene) this contig may
-    support
-    """
-    if is_right_tail_segment(contig):
-        return '+'
-    elif is_left_tail_segment(contig):
-        return '-'
-    else:
-        raise ValueError(f'cannot infer strand for contig: {contig.query_name}')
-
-
-def gen_contig_info(contig):
-    return [
-        contig.reference_name,
-        calc_strand(contig),
-        contig.query_name,
-        contig.query_length,
-        contig.mapq
-    ]
-
-
-def analyze_tail_contig(contig):
-    contig_info = gen_contig_info(contig)
-    ref_clv = calc_ref_clv(contig)
-    num_tail_reads, _ = calc_num_tail_reads(contig, r2c_sam)
-    tail_length = calc_tail_length(contig)
-
-    clv_record = (
-        *contig_info, ref_clv, 'tail_contig',
-        # bridge_contig evidence is left empty
-        num_tail_reads, tail_length, 0, 0, 0, 0
-    )
-    return clv_record
-
-
-def is_tail_segment(segment):
-    return is_right_tail_segment(segment) or is_left_tail_segment(segment)
-
-
-def is_right_tail_segment(segment, tail_base='A'):
-    """
-    tseg: tail segment
-
-    default tail_base applies main to alignment to genome, where the polyA tail
-    strand is known
-    """
-    seq = segment.query_sequence
-    last_cigar = segment.cigartuples[-1]
-    # potential test case == "A0.R100820":
-    return (
-        seq.endswith(tail_base)
-        # right clipped
-        and last_cigar[0] == BAM_CSOFT_CLIP
-        # clipped are all As
-        and set(seq[-last_cigar[1]:]) == {tail_base}
-    )
-
-
-def is_left_tail_segment(segment, tail_base='T'):
-    seq = segment.query_sequence
-    first_cigar = segment.cigartuples[0]
-    # potential test case A0.S36384
-    return (
-        seq.startswith(tail_base)
-        # left clipped
-        and first_cigar[0] == BAM_CSOFT_CLIP
-        # clipped are all Ts
-        and set(seq[:first_cigar[1]]) == {tail_base}
-    )
-
-
-def calc_ref_clv(segment):
-    """calculate cleavage site position wst the reference"""
-    if is_right_tail_segment(segment):      # + strand
-        return segment.reference_end + 1
-    elif is_left_tail_segment(segment):  # - strand
-        return segment.reference_start
-    else:
-        raise
-
-
-def calc_tail_length(segment):
-    """
-    calculate A/T length of a tail contig, this information is extracted from
-    softclip in the CIGAR
-    """
-    if is_right_tail_segment(segment):
-        return segment.cigartuples[-1][1]
-    elif is_left_tail_segment(segment):
-        return segment.cigartuples[0][1]
-    else:
-        return '?'
-
-
-def calc_num_tail_reads(segment, r2c_sam):
-    """
-    calculate the number of reads aligned to the cleavage site
-
-    https://pysam.readthedocs.io/en/latest/api.html?highlight=AlignmentSegment#pysam.AlignedSegment.cigartuples
-    """
-    if is_right_tail_segment(segment):
-        # clv position wst. the segment
-        seg_clv = segment.query_alignment_end
-    elif is_left_tail_segment(segment):
-        seg_clv = segment.query_alignment_start - 1
-    else:
-        raise
-
-    num_tail_reads = r2c_sam.count(
-        # region is half-open
-        # https://pysam.readthedocs.io/en/latest/glossary.html#term-region
-        segment.query_name, seg_clv, seg_clv + 1
-    )
-    return num_tail_reads, seg_clv
 
 
 def infer_contig_abs_ref_start(contig):
@@ -179,8 +62,8 @@ if __name__ == "__main__":
     # datadir = '/projects/btl/zxue/tasrkleat-TCGA-results/tasrkleat-TCGA-analysis-scripts/benchmark-kleat.bk/HBR/C4/tasrkleat-results'
     # datadir = '/projects/btl/zxue/tasrkleat-TCGA-results/tasrkleat-TCGA-analysis-scripts/benchmark-kleat.bk/HBR/C6/tasrkleat-results'
 
-    c2g_sam = pysam.AlignmentFile(f'{datadir}/align_contigs2genome/cba.sorted.bam')
-    r2c_sam = pysam.AlignmentFile(f'{datadir}/align_reads2contigs/cba.sorted.bam')
+    c2g_bam = pysam.AlignmentFile(f'{datadir}/align_contigs2genome/cba.sorted.bam')
+    r2c_bam = pysam.AlignmentFile(f'{datadir}/align_reads2contigs/cba.sorted.bam')
 
     import sys
     output = sys.argv[1]
@@ -199,7 +82,7 @@ if __name__ == "__main__":
             'num_blank_contigs'   # those contigs that provide no polya evidence
         ])
 
-        for k, contig in enumerate(c2g_sam):
+        for k, contig in enumerate(c2g_bam):
             if (k + 1) % 1000 == 0:
                 print(f'processed {k + 1} contigs')
 
@@ -211,8 +94,8 @@ if __name__ == "__main__":
             ):
                 continue
 
-            if is_tail_segment(contig):
-                clv_record = analyze_tail_contig(contig)
+            if is_suffix_contig(contig):
+                clv_record = analyze_suffix_contig(contig, r2c_bam)
                 csvwriter.writerow(clv_record)
             # else:
             #     contig_is_blank = True
