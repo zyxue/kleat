a602540c3eb081d119a6df6dacf4826f166c9476
Zhuyi Xue <alfred532008@gmail.com>
2018-07-01 09:39:10 -0700

    debugged + strand reverse contig with skipped region

--- evidence/bridge.py
+++ evidence/bridge.py
@@ -2,18 +2,20 @@ import utils as U
 from settings import ClvRecord, BAM_CMATCH, BAM_CREF_SKIP
 
 
-def calc_offset(contig, stop_match_pos):
+def calc_offset(contig, match_len_cutoff):
     """
     Calculate the offset caused by skipped region (e.g. intron) until stop_pos
     relative to contig
     """
-    match_pos = 0
+    match_len = 0
     offset = 0
     for key, val in contig.cigartuples:
-        if key == BAM_CMATCH:
-            match_pos += val
-            if match_pos > stop_match_pos:
-                return offset
+        if key in [BAM_CMATCH]:
+            match_len += val
+            if match_len >= match_len_cutoff:
+                delta = val - (match_len - match_len_cutoff)
+                offset += delta
+                break
             offset += val
         if key == BAM_CREF_SKIP:
             offset += val
@@ -21,44 +23,47 @@ def calc_offset(contig, stop_match_pos):
 
 
 def analyze_bridge_read(contig, read):
-    ctg_beg = U.infer_contig_abs_ref_start(contig)
-    ctg_end = U.infer_contig_abs_ref_end(contig)
+    # beginning and end wst to genome
+    gnm_beg = U.infer_contig_abs_ref_start(contig)
+    gnm_end = U.infer_contig_abs_ref_end(contig)
 
     seqname = contig.reference_name
     if not contig.is_reverse:
         if U.left_tail(read, 'T'):
             strand = '-'
-            contig_clv = read.reference_start  # in contig coordinate
-            ref_clv = ctg_beg + contig_clv     # convert to genome coordinate
+            ctg_clv = read.reference_start  # in contig coordinate
+            gnm_clv = gnm_beg + ctg_clv     # convert to genome coordinate
             tail_len = read.cigartuples[0][1]
         elif U.right_tail(read, 'A'):
             strand = '+'
             stop_match_pos = read.reference_end
-            ref_clv = ctg_beg + calc_offset(contig, stop_match_pos) + 1
+            offset = calc_offset(contig, stop_match_pos)
+            gnm_clv = gnm_beg + offset + 1
             tail_len = read.cigartuples[-1][1]
         else:
             raise ValueError(f'no tail found for read {read}')
     else:
         if U.left_tail(read, 'T'):
             strand = '+'
-            contig_clv = read.reference_start
-            ref_clv = ctg_end - contig_clv
+            match_len_cutoff = contig.query_length - read.reference_start
+            offset = calc_offset(contig, match_len_cutoff)
+            gnm_clv = gnm_beg + offset + 1
             tail_len = read.cigartuples[0][1]
         elif U.right_tail(read, 'A'):
             strand = '-'
             contig_clv = read.reference_end
-            ref_clv = ctg_end - contig_clv
+            gnm_clv = gnm_end - contig_clv
             tail_len = read.cigartuples[-1][1]
         else:
             raise ValueError(f'no tail found for read {read}')
-    return seqname, strand, ref_clv, tail_len
+    return seqname, strand, gnm_clv, tail_len
 
 
 def gen_clv_record(
         bridge_contig, clv_key_tuple, num_bridge_reads, max_bridge_tail_len):
-    seqname, strand, ref_clv = clv_key_tuple
+    seqname, strand, gnm_clv = clv_key_tuple
     return ClvRecord(
-        seqname, strand, ref_clv,
+        seqname, strand, gnm_clv,
 
         'bridge',
         bridge_contig.query_name,
