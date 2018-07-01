3d82c532337ec68e9b96b2445ddd053da961ea96
Zhuyi Xue <alfred532008@gmail.com>
2018-07-01 11:12:55 -0700

    cleaned kleat3.py; removed contig_is_blank flag
    For simplicity, alway check for 4 types of evidence for each contig

--- kleat3.py
+++ kleat3.py
@@ -29,19 +29,17 @@ if __name__ == "__main__":
         csvwriter.writerow(HEADER)
 
         for k, contig in enumerate(c2g_bam):
-            contig_is_blank = True
             if (k + 1) % 1000 == 0:
                 print(f'processed {k + 1} contigs')
 
             tmp_dd[contig.query_name] = contig
 
-            if (contig.is_unmapped or contig.mapping_quality == 0):
+            if contig.is_unmapped:
                 continue
 
             # suffix evidence
             if U.has_tail(contig):
                 clv_record = suffix.gen_clv_record(contig, r2c_bam)
-                contig_is_blank = False
                 U.write_row(clv_record, csvwriter)
 
             dd_bridge = {
@@ -53,11 +51,8 @@ if __name__ == "__main__":
                 'num_reads': defaultdict(int)
             }
 
-            query_region = [0, contig.query_length]
-            for read in r2c_bam.fetch(contig.query_name, *query_region):
-                # if read.query_name == "SN7001282:314:h15b0adxx:1:2102:16317:20542" and read.is_unmapped:
-                #     sys.exit(1)
-
+            for read in r2c_bam.fetch(contig.query_name):
+                # bridge evidence
                 if not read.is_unmapped:
                     if U.has_tail(read):
                         seqname, strand, ref_clv, tail_len = \
@@ -67,6 +62,7 @@ if __name__ == "__main__":
                         dd_bridge['num_reads'][clv_key] += 1
                         dd_bridge['max_tail_len'][clv_key] = max(
                             dd_bridge['max_tail_len'][clv_key], tail_len)
+                # link evidence
                 else:
                     # Here we focused on the unmapped all A/T read, but in
                     # principle, we could also check from the perspecitve and
@@ -84,7 +80,6 @@ if __name__ == "__main__":
                         dd_link['num_reads'][clv_key] += 1
 
             if len(dd_bridge['num_reads']) > 0:
-                contig_is_blank = False
                 for clv_key in dd_bridge['num_reads']:
                     clv_record = bridge.gen_clv_record(
                         contig, clv_key,
@@ -94,12 +89,11 @@ if __name__ == "__main__":
                     U.write_row(clv_record, csvwriter)
 
             if len(dd_link['num_reads']) > 0:
-                contig_is_blank = False
                 for ref_clv in dd_link['num_reads']:
                     clv_record = link.gen_clv_record(
                         contig, clv_key, dd_link['num_reads'][clv_key])
                     U.write_row(clv_record, csvwriter)
 
-            if contig_is_blank:
-                for clv_rec in blank.gen_two_clv_records(contig):
-                    U.write_row(clv_rec, csvwriter)
+            # blank evidence
+            for clv_rec in blank.gen_two_clv_records(contig):
+                U.write_row(clv_rec, csvwriter)
