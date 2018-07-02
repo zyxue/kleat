f2933692b5752a6db70912b27284fe577c0c24c2
Zhuyi Xue <alfred532008@gmail.com>
2018-07-02 10:04:26 -0700

    find packages recursively
    https://stackoverflow.com/questions/10924885/is-it-possible-to-include-subdirectories-using-dist-utils-setup-py-as-part-of

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     include_package_data=True,
     keywords='kleat',
     name='kleat',
-    packages=find_packages(include=['kleat']),
+    packages=find_packages(),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
