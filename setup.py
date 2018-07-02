6f97f2d21d3eb0b1d4fab64b5d199b200295cb0d
Zhuyi Xue <alfred532008@gmail.com>
2018-07-02 08:44:07 -0700

    added setup.cfg setup.py

---
+++ setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['Click>=6.0', 'pysam>=0.14.1']
+
+setup_requirements = ['pytest-runner', ]
+
+test_requirements = ['pytest', ]
+
+setup(
+    author="Zhuyi Xue",
+    author_email='zxue@bcgsc.ca',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    description="Cleavage site prediction via de novo assembly",
+    entry_points={
+        'console_scripts': [
+            'kleat3=kleat3.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='kleat3',
+    name='kleat3',
+    packages=find_packages(include=['kleat3']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/zyxue/kleat3',
+    version='3.0.0',
+    zip_safe=False,
+)
