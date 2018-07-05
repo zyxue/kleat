d9394309c5c2bfa5b068a26afc40262533964f3a
Zhuyi Xue <alfred532008@gmail.com>
2018-07-05 15:46:31 -0700

    tested analyze_reverse_link

--- tests/test_evidence/test_link.py
+++ tests/test_evidence/test_link.py
@@ -38,3 +38,38 @@ def test_analyze_forward_link_for_polyT_read():
 
     assert link.analyze_forward_link(c, r) == ('-', 1)
 
+
+def test_analyze_reverse_link_for_polyA_read():
+    """
+    polyA read is polyT before reverse
+
+    TTT-------CCG   <-right-tail read
+             XCCGXX <-contig
+            0123456 <-genome coord
+    """
+    r = MagicMock()
+    r.query_sequence = 'AAA'
+
+    c = MagicMock()
+    c.reference_start = 0
+    c.reference_end = 7
+
+    assert link.analyze_reverse_link(c, r) == ('-', 1)
+
+
+def test_analyze_reverse_link_for_polyT_reads():
+    """
+    polyT read is polyA before reverse
+
+      CCG-------AAA   <-right-tail read
+     XCCGXX           <-contig
+    0123456           <-genome coord
+    """
+    r = MagicMock()
+    r.query_sequence = 'TTT'
+
+    c = MagicMock()
+    c.reference_start = 0
+    c.reference_end = 6
+
+    assert link.analyze_reverse_link(c, r) == ('+', 6)
