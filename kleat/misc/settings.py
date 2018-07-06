274cbc4910cdbf528d823cdbc1c5c51e3d00edac
Zhuyi Xue <alfred532008@gmail.com>
2018-07-06 15:41:08 -0700

    added four seqname-related constants to settings.py

--- kleat/misc/settings.py
+++ kleat/misc/settings.py
@@ -60,6 +60,24 @@ CANDIDATE_HEXAMERS = [
 ]
 
 
+UCSC_SEQNAMES = [
+    'chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9', 'chr10',
+    'chr11', 'chr12', 'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19', 'chr20',
+    'chr21', 'chr22',
+    'chrX', 'chrY', 'chrM'
+]
+
+ENSEMBL_SEQNAMES = [
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
+    '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+    '21', '22',
+    'X', 'Y', 'MT'
+]
+
+UCSC_TO_ENSEMBL_SEQNAME = dict(zip(UCSC_SEQNAMES, ENSEMBL_SEQNAMES))
+ENSEMBL_TO_UCSC_SEQNAME = dict(zip(ENSEMBL_SEQNAMES, UCSC_SEQNAMES))
+
+
 # # kept for reference
 # COMPLEMENT_DICT = str.maketrans("ACTG", "TGAC")
 
