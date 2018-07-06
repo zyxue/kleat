26e4f9991e58ee060666166fa253e6b8bd58cf92
Zhuyi Xue <alfred532008@gmail.com>
2018-07-06 15:40:39 -0700

    added timeit to utils.py

--- kleat/misc/utils.py
+++ kleat/misc/utils.py
@@ -1,10 +1,32 @@
 import os
 import logging
+import time
+from functools import update_wrapper
 
 logger = logging.getLogger(__name__)
 
 
 """Below are very generic functions not related to APA"""
+
+
+def decorator(d):
+    "Make function d a decorator: d wraps a function fn."
+    def _d(fn):
+        return update_wrapper(d(fn), fn)
+    update_wrapper(_d, d)
+    return _d
+
+
+@decorator
+def timeit(f):
+    """time a function, used as decorator"""
+    def new_f(*args, **kwargs):
+        bt = time.time()
+        r = f(*args, **kwargs)
+        et = time.time()
+        logger.info("time spent on {0}: {1:.2f}s".format(f.__name__, et - bt))
+        return r
+    return new_f
 
 
 def backup_file(f):
