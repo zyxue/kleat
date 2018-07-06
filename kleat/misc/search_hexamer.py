56834f449a489ea7cd8bc69ea1f1fa9a4e5fd971
Zhuyi Xue <alfred532008@gmail.com>
2018-07-06 10:26:07 -0700

    fixed bug in extract_seq

--- kleat/misc/search_hexamer.py
+++ kleat/misc/search_hexamer.py
@@ -116,13 +116,18 @@ def extract_seq(contig):
     """remove clipped ends before searching for hexamer, the clipped ends would
     affect calculation of genomics coordinates of the found PAS hexamer"""
     beg_offset = 0
-    end_offset = -1
+    end_offset = None
     for key, val in contig.cigartuples:
         if key == S.BAM_CSOFT_CLIP:
             beg_offset = val
         if key == S.BAM_CHARD_CLIP:
             end_offset = val
-    return contig.query_sequence[beg_offset: end_offset]
+
+    seq = contig.query_sequence
+    if end_offset is None:
+        return seq[beg_offset:end_offset]
+    else:
+        return seq[beg_offset:]
 
 
 def gen_contig_hexamer_tuple(contig, strand, ref_clv):
