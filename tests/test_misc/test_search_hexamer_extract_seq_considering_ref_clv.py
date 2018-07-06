4c2c1539d1121f02ff0a62f5ac0fc477f9c7d5f2
Zhuyi Xue <alfred532008@gmail.com>
2018-07-06 12:26:34 -0700

    added TODO test_extract_seq_where_ref_clv_is_not_at_the_end_of_the_contig_for_minus_strand_clv

--- tests/test_misc/test_search_hexamer_extract_seq_considering_ref_clv.py
+++ tests/test_misc/test_search_hexamer_extract_seq_considering_ref_clv.py
@@ -7,6 +7,22 @@ import kleat.misc.settings as S
 from kleat.misc.search_hexamer import extract_seq
 
 
+# TODO
+# def test_extract_seq_where_ref_clv_is_not_at_the_end_of_the_contig_for_minus_strand_clv():
+#     """
+#     TT
+#     |└AC TTTATT   <-bridge read
+#     AACGG┘||||└CG <-bridge contig
+#     0123456789012 <-contig coord
+#     7890123456789 <-genome coordinate
+#       ^ref_clv
+#     """
+#     c = MagicMock()
+#     c.query_sequence = 'AACGGTTTATT'
+#     c.cigartuples = ((S.BAM_CMATCH, 13))
+#     assert extract_seq(c) == 'CGGTTTATT'
+
+
 class TestExtractSeqForSoftClippedSeq(unittest.TestCase):
     def test_extract_seq_with_starting_softclip(self):
         c = MagicMock()
