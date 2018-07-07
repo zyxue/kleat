3314c6345e7b766bf86c17050bbc2e090c26a739
Zhuyi Xue <alfred532008@gmail.com>
2018-07-07 08:09:05 -0700

    updated train_arbor.py for transcriptome benchmark

--- train_arbor.py
+++ train_arbor.py
@@ -1,21 +1,29 @@
 import os
 import sys
-import pickle
 import csv
+import logging
 
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 
-from ml_utils import load_polya_seq_df_114genes, map_clvs, compare, KARBOR_FEATURE_COLS
+from ml_utils import load_polya_seq_df, map_clvs, compare, KARBOR_FEATURE_COLS
 from cluster import cluster_clv_sites
+from kleat.misc.utils import backup_file
+
+
+logging.basicConfig(
+    level=logging.DEBUG, format='%(asctime)s|%(levelname)s|%(message)s')
 
 
 def load_df(sample_id):
-    return pd.read_csv(f'./benchmark_114genes/karbor_ml/{sample_id}_ml_ready.csv', low_memory=False)
+    infile = os.path.abspath(
+        './benchmark_transcriptome/{0}.ml_ready.pkl'.format(sample_id))
+    logging.info('reading {0} ...'.format(infile))
+    return pd.read_pickle(infile)
 
 
 def map_df(df, sample_id, mapping_cutoff=50):
-    df_ref = load_polya_seq_df_114genes(sample_id)
+    df_ref = load_polya_seq_df(sample_id)
 
     df = map_clvs(df, df_ref)
     df['is_tp'] = df.abs_dist < mapping_cutoff
@@ -26,6 +34,7 @@ def train(df_tr_mapped, max_depth):
     Xs = df_tr_mapped[KARBOR_FEATURE_COLS]
     ys = df_tr_mapped.is_tp.values
     clf = DecisionTreeClassifier(max_depth=max_depth)
+    logging.info('training in process ...')
     clf.fit(Xs, ys)
     return clf
 
@@ -51,16 +60,21 @@ def cluster_clv(df, cutoff=20):
 
 
 MAP_CUTOFF = 50
-SAMPLE_IDS = ['HBRC4', 'HBRC6', 'UHRC1', 'UHRC2']
-
+SAMPLE_IDS = [
+    'HBRC4',
+    'HBRC6',
+    'UHRC1',
+    'UHRC2'
+]
 
 ref_sample_id = sys.argv[1]
 df_tr = load_df(ref_sample_id)
 df_tr_mapped = map_df(df_tr, ref_sample_id)
 
-
-max_depth_list = range(2, 20, 1)
-with open('./out.csv', 'wt') as opf:
+max_depth_list = range(2, 10, 1)
+outfile = './out.csv'
+backup_file(outfile)
+with open(outfile, 'wt') as opf:
     csvwriter = csv.writer(opf)
     csvwriter.writerow(
         ['sample_id', 'precision', 'recall', 'f1', 'tree_max_depth']
@@ -73,7 +87,7 @@ with open('./out.csv', 'wt') as opf:
             print(f'testing on {test_sample_id}')
             df_predicted = predict(test_sample_id, clf)
             df_clustered = cluster_clv(df_predicted)
-            df_ref = load_polya_seq_df_114genes(test_sample_id)
+            df_ref = load_polya_seq_df(test_sample_id)
 
             reca, prec, f1 = compare(df_clustered, df_ref)
             csvwriter.writerow(
