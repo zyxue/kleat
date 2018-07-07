5a3e63e982c35bb02e6c10f8acb963ae511407b2
Zhuyi Xue <alfred532008@gmail.com>
2018-07-06 18:22:49 -0700

    made train/predict/cluster/compare altogether

--- train_arbor.py
+++ train_arbor.py
@@ -1,40 +1,81 @@
 import os
 import sys
 import pickle
+import csv
 
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 
-from ml_utils import load_polya_seq_df, map_clvs, KARBOR_FEATURE_COLS
+from ml_utils import load_polya_seq_df_114genes, map_clvs, compare, KARBOR_FEATURE_COLS
+from cluster import cluster_clv_sites
 
 
-kleat_version = 'karbor'
-map_cutoff = 50
-sample_id = sys.argv[1]
+def load_df(sample_id):
+    return pd.read_csv(f'./benchmark_114genes/karbor_ml/{sample_id}_ml_ready.csv', low_memory=False)
 
-fdf = pd.read_csv(f'./benchmark_114genes/{kleat_version}_ml/{sample_id}_ml_ready.csv', low_memory=False)
 
-df_ref = load_polya_seq_df(sample_id)
+def map_df(df, sample_id, mapping_cutoff=50):
+    df_ref = load_polya_seq_df_114genes(sample_id)
 
-fdf_mapped = map_clvs(fdf, df_ref)
+    df = map_clvs(df, df_ref)
+    df['is_tp'] = df.abs_dist < mapping_cutoff
+    return df
 
-# the row orders are different from fdf
-fdf_mapped['is_tp'] = fdf_mapped.abs_dist < map_cutoff
 
-Xs = fdf_mapped[KARBOR_FEATURE_COLS]
-
-depths = range(2, 20, 1)
-for max_depth in depths:
-    print(f'working on depth: {max_depth}')
+def train(df_tr_mapped, max_depth):
+    Xs = df_tr_mapped[KARBOR_FEATURE_COLS]
+    ys = df_tr_mapped.is_tp.values
     clf = DecisionTreeClassifier(max_depth=max_depth)
-    clf.fit(Xs, fdf_mapped.is_tp.values)
+    clf.fit(Xs, ys)
+    return clf
 
-    fdf['predicted'] = clf.predict(Xs)
 
-    outdir = f'./benchmark_114genes/{kleat_version}_ml/pkl/DT/{sample_id}/max_depth{max_depth}'
-    if not os.path.exists(outdir):
-        os.makedirs(outdir)
-    out_pkl = f'{outdir}/clf.pkl'
-    print(f'writing {out_pkl}')
-    with open(out_pkl, 'wb') as opf:
-        pickle.dump(clf, opf)
+def predict(test_sample_id, clf):
+    df_te = load_df(test_sample_id)
+    df_te_mapped = map_df(df_te, test_sample_id)
+
+    Xs_te = df_te_mapped[KARBOR_FEATURE_COLS]
+    df_te_mapped['predicted'] = clf.predict(Xs_te)
+
+    clv_key_cols = ['seqname', 'strand', 'clv']
+    out_df = df_te_mapped.query('predicted')[clv_key_cols].drop_duplicates()
+    return out_df
+
+
+def cluster_clv(df, cutoff=20):
+    grped = df.groupby(['seqname', 'strand'])
+    applied = grped.apply(cluster_clv_sites, cutoff)
+    dedupped = applied[['seqname', 'strand', 'mclv']].drop_duplicates()
+    out = dedupped.rename(columns={'mclv': 'clv'}).reset_index(drop=True)
+    return out
+
+
+MAP_CUTOFF = 50
+SAMPLE_IDS = ['HBRC4', 'HBRC6', 'UHRC1', 'UHRC2']
+
+
+ref_sample_id = sys.argv[1]
+df_tr = load_df(ref_sample_id)
+df_tr_mapped = map_df(df_tr, ref_sample_id)
+
+
+max_depth_list = range(2, 20, 1)
+with open('./out.csv', 'wt') as opf:
+    csvwriter = csv.writer(opf)
+    csvwriter.writerow(
+        ['sample', 'precision', 'recall', 'F1', 'tree_max_depth']
+    )
+    for max_depth in max_depth_list:
+        print(f'working on depth: {max_depth}')
+        clf = train(df_tr_mapped, max_depth)
+
+        for test_sample_id in SAMPLE_IDS:
+            df_predicted = predict(test_sample_id, clf)
+            df_clustered = cluster_clv(df_predicted)
+            df_ref = load_polya_seq_df_114genes(test_sample_id)
+            pr, re, f1 = compare(df_clustered, df_ref)
+
+            csvwriter.writerow(
+                [test_sample_id, pr, se, f1, max_depth]
+            )
+        break
