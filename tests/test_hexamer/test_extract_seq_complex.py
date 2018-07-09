9a03d3f5d76d21d6a0ffc83a54e98c10c3dd2259
Zhuyi Xue <alfred532008@gmail.com>
2018-07-08 20:21:10 -0700

    added a test case with both skip and del for + strand extract_seq

--- tests/test_hexamer/test_extract_seq_complex.py
+++ tests/test_hexamer/test_extract_seq_complex.py
@@ -28,6 +28,27 @@ def test_extract_seq_with_skipped_region_for_plus_strand_clv():
     ref_fa.fetch.assert_called_with('chr1', 11, 13)
 
 
+def test_extract_seq_with_deleted_region_for_plus_strand_clv():
+    """
+               AAA             <-tail of suffix contig
+       ACGG--GC┘||             <-suffix contig with skip
+       0123  456789            <-contig coord
+              |  1
+       ctg_clv^  ^init_ctg_idx <-contig coord
+    ...ACGGTTGCGGT...          <-genome
+       789012345678            <-genome coord
+          1   |  |
+       ref_clv^  ^init_ref_idx
+    """
+    ctg = MagicMock()
+    ctg.reference_name = 'chr1'
+    ctg.query_sequence = 'ACGGGCAAA'  # len 9
+    ctg.cigartuples = ((S.BAM_CMATCH, 4), (S.BAM_CDEL, 2), (S.BAM_CMATCH, 2), (S.BAM_CSOFT_CLIP, 3))
+
+    ref_fa = MagicMock()
+    assert extract_seq(contig=ctg, strand='+', ref_clv=14, ref_fa=ref_fa, ctg_clv=5) == 'ACGGGC'
+
+
 def test_extract_seq_with_skipped_region_for_minus_strand_clv():
     """
     cc: ctg_clv; ici: init_clv_id
@@ -116,6 +137,36 @@ def test_extract_seq_with_two_skipped_region_for_plus_strand_clv():
     ref_fa.fetch.assert_has_calls([call('chr3', 11, 13), call('chr3', 8, 9)])
 
 
+def test_extract_seq_with_skipped_and_deleted_regions_for_plus_strand_clv():
+    """
+               AAA             <-tail of suffix contig
+       A-TT--GC┘||             <-suffix contig with skip
+       0 12  345678            <-contig coord
+              |
+       ctg_clv^  ^init_ctg_idx <-contig coord
+    ...ACTTAAGCGGT...          <-genome
+       789012345678            <-genome coord
+          1   |  |
+       ref_clv^  ^init_ref_idx
+    """
+    ctg = MagicMock()
+    ctg.reference_name = 'chr3'
+    ctg.query_sequence = 'ATTGCAAA'
+    ctg.cigartuples = (
+        (S.BAM_CMATCH, 1),
+        (S.BAM_CDEL, 1),
+        (S.BAM_CMATCH, 2),
+        (S.BAM_CREF_SKIP, 2),
+        (S.BAM_CMATCH, 2),
+        (S.BAM_CSOFT_CLIP, 3),
+    )
+
+    ref_fa = MagicMock()
+    ref_fa.fetch.return_value = 'AA'
+    assert extract_seq(contig=ctg, strand='+', ref_clv=14, ref_fa=ref_fa, ctg_clv=4) == 'ATTAAGC'
+    ref_fa.fetch.assert_called_once_with('chr3', 11, 13)
+
+
 def test_extract_seq_with_three_skipped_region_and_mismatches_for_plus_strand_clv():
     """
                      AA             <-tail of suffix contig
@@ -147,6 +198,7 @@ def test_extract_seq_with_three_skipped_region_and_mismatches_for_plus_strand_cl
     assert extract_seq(contig=ctg, strand='+', ref_clv=20, ref_fa=ref_fa, ctg_clv=7) == 'ACTGCCAGTATTGC'
     assert ref_fa.fetch.call_count == 3
     ref_fa.fetch.assert_has_calls([call('chr3', 17, 19), call('chr3', 13, 14), call('chr3', 8, 11)])
+
 
 
 # def test_extract_seq_where_for_plus_strand_clv_supported_by_suffix_with_specified_window_size():
