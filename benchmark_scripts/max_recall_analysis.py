f776f08a497bbfbc58de551cdd80dd746ad26ede
Zhuyi Xue <alfred532008@gmail.com>
2018-07-12 12:57:41 -0700

    log reading input in max_recall_analysis.py

--- benchmark_scripts/max_recall_analysis.py
+++ benchmark_scripts/max_recall_analysis.py
@@ -10,9 +10,9 @@ from train_arbor import cluster_clv, map_to_ref, load_polya_seq_df
 
 
 sample_id = sys.argv[1]
-
-adf = pd.read_csv(f'./benchmark_transcriptome/before_chimeric/{sample_id}.tsv',
-                  keep_default_na=False, sep='\t')
+infile = f'./benchmark_transcriptome/before_chimeric/{sample_id}.tsv'
+print(f'reading {infile}')
+adf = pd.read_csv(infile, keep_default_na=False, sep='\t')
 print(f'before clustering df.shape: {adf.shape}')
 
 print('clustering ...')
