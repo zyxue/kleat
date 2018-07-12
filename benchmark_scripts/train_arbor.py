ad0777db1ad5998ed33d5cba6cab2f701542c25c
Zhuyi Xue <alfred532008@gmail.com>
2018-07-12 09:09:59 -0700

    added get_args to and refactored train_arbor.py

--- benchmark_scripts/train_arbor.py
+++ benchmark_scripts/train_arbor.py
@@ -1,6 +1,7 @@
 import os
-import sys
+import argparse
 import csv
+import pickle
 import logging
 import multiprocessing
 
@@ -18,14 +19,16 @@ logging.basicConfig(
 
 def load_df(sample_id):
     infile = os.path.abspath(
-        './benchmark_transcriptome/{0}.ml_ready.pkl'.format(sample_id))
-    logging.info('reading {0} ...'.format(infile))
-    return pd.read_pickle(infile)
+        f'./benchmark_transcriptome/{sample_id}.ml_ready.pkl')
+    logging.info(f'reading {infile} ...')
+    df = pd.read_pickle(infile)
+    return df
 
 
 def map_df(df, sample_id, mapping_cutoff=50):
     df_ref = load_polya_seq_df(sample_id)
 
+    logging.info('mapping predicted clv to ground truth ...')
     df = map_clvs(df, df_ref)
     df['is_tp'] = df.abs_dist < mapping_cutoff
     return df
@@ -67,7 +70,7 @@ def cluster_clv(df, cutoff=20):
     return out
 
 
-def run_test(sample_id, df_te_mapped, clf):
+def run_test(test_sample_id, df_te_mapped, df_ref, clf, map_cutoff):
     """args as list, otherwise this function can't be passed
     multiprocessing.Pool"""
     logging.info('testing on {0} with tree max_depth={1}'.format(
@@ -75,59 +78,132 @@ def run_test(sample_id, df_te_mapped, clf):
 
     df_predicted = predict(df_te_mapped, clf)
     df_clustered = cluster_clv(df_predicted)
-    df_ref = load_polya_seq_df(test_sample_id)
 
-    recall, precision, f1 = compare(df_clustered, df_ref)
-    return sample_id, precision, recall, f1, clf.max_depth
+    recall, precision, f1 = compare(df_clustered, df_ref, map_cutoff)
+    return test_sample_id, precision, recall, f1, clf.max_depth
 
 
 def run_test_wrapper(args):
     return run_test(*args)
 
 
-if __name__ == "__main__":
-    MAP_CUTOFF = 50
-    NUM_CPUS = 16
-    TEST_SAMPLE_IDS = [
-        'HBRC4',
-        'HBRC6',
-        'UHRC1',
-        'UHRC2'
-    ]
+def run_test_in_parallel(clf_list, test_sample_ids, map_cutoff, num_cpus):
+    for test_sample_id in test_sample_ids:
+        df_te = load_df(test_sample_id)
+        df_te_mapped = map_df(df_te, test_sample_id)
+        df_ref = load_polya_seq_df(test_sample_id)
 
-    ref_sample_id = sys.argv[1]
+        args_list_for_test = []
+        for clf in clf_list:
+            args_list_for_test.append(
+                (test_sample_id, df_te_mapped, df_ref, clf, map_cutoff)
+            )
 
-    df_tr = load_df(ref_sample_id)
-    df_tr_mapped = map_df(df_tr, ref_sample_id)
+        with multiprocessing.Pool(num_cpus) as p:
+            logging.info(f'start parallel testing with {num_cpus} CPUs ...')
+            results = p.map(run_test_wrapper, args_list_for_test)
+    return results
 
-    max_depth_list = range(2, 20, 1)
+
+def gen_clf_out_pkl(output, depth):
+    """
+    generate path to output clf pickl based on output_csv and depth of the tree
+    """
+    out_pkl = os.path.join(
+        os.path.dirname(output),
+        os.path.basename(output).replace('.csv', f'.tree_max_depth_{depth}.pkl')
+    )
+    return out_pkl
+
+
+def pickle_clf(clf, out_pkl):
+    if os.path.exists(out_pkl):
+        backup_file(out_pkl)
+    with open(out_pkl, 'wb') as opf:
+        pickle.dump(clf, opf)
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description=('train & test a decision tree for different max_depths '
+                     'for UHRC1/2 and HBRC4/6 samples.')
+    )
+
+    parser.add_argument(
+        '-r', '--train-sample-id', type=str, required=True,
+        help='the sample ID to train a decision tree on, e.g. HBRC4'
+    )
+    parser.add_argument(
+        '-e', '--test-sample-ids', type=str, nargs='+', required=True,
+        help='the sample IDs to test the tree on, e.g. UHRC1 UHRC2 HBRC4 HBRC6'
+    )
+    parser.add_argument(
+        '-d', '--max-depths', type=int, nargs=3, default=[2, 20, 1],
+        help='the range of max_depth values for training the tree, [beg, end, step]'
+    )
+    parser.add_argument(
+        '-p', '--pickle-depths', type=int, nargs='+', default=[],
+        help=('for the particular max_depth values, '
+              'the tree would be pickled and a visualization would be produced ')
+    )
+    parser.add_argument(
+        '-o', '--output', type=str, default='./out.csv',
+        help='output for recording testing results in csv'
+    )
+
+    # arguments below are often good in default
+    parser.add_argument(
+        '-c', '--map-cutoff', type=int, default=50,
+        help=('the cutoff below which the predicted clv and the reference clv '
+              '(e.g. polyA-Seq) are considered the same')
+    )
+    parser.add_argument(
+        '-t', '--num-cpus', type=int,
+        default=min(multiprocessing.cpu_count(), 16),
+        help='the number of CPUs to use for parallel training and testing'
+    )
+    return parser.parse_args()
+
+
+def main():
+
+    args = get_args()
+    train_sample_id = args.train_sample_id
+    test_sample_ids = args.test_sample_ids
+
+    df_tr = load_df(train_sample_id)
+    df_tr_mapped = map_df(df_tr, train_sample_id)
+
+    beg, end, step = args.max_depths
+    max_depth_list = range(beg, end, step)
+    logging.info(f'max_depth list trees: {max_depth_list}')
     train_args = prepare_args(df_tr_mapped, max_depth_list)
 
-    with multiprocessing.Pool(NUM_CPUS) as p:
-        logging.info('start training in parallel '
-                     'with {0} CPUs ...'.format(NUM_CPUS))
+    logging.info('prepare for TRAINing ...')
+    with multiprocessing.Pool(args.num_cpus) as p:
+        logging.info(f'start parallel training with {args.num_cpus} CPUs ...')
         clf_list = p.map(train_it_wrapper, train_args)
 
-    outfile = './out.csv'
-    backup_file(outfile)
-    with open(outfile, 'wt') as opf:
+    clf_dd = dict(zip(max_depth_list, clf_list))
+    for depth in clf_dd:
+        if depth in args.pickle_depths:
+            out_pkl = gen_clf_out_pkl(args.output, depth)
+            pickle_clf(clf_dd[depth], out_pkl)
+
+    logging.info('prepare for TESTing ...')
+    backup_file(args.output)
+    with open(args.output, 'wt') as opf:
         csvwriter = csv.writer(opf)
         csvwriter.writerow(
             ['sample_id', 'precision', 'recall', 'f1', 'tree_max_depth']
         )
-        for test_sample_id in TEST_SAMPLE_IDS:
-            if test_sample_id == ref_sample_id:
-                df_te_mapped = df_tr_mapped
-            else:
-                df_te = load_df(test_sample_id)
-                df_te_mapped = map_df(df_te, test_sample_id)
 
-            args_list_for_test = []
-            for clf in clf_list:
-                args_list_for_test.append((test_sample_id, df_te_mapped, clf))
+        test_results = run_test_in_parallel(
+            clf_list, test_sample_ids, args.map_cutoff, args.num_cpus)
 
-            with multiprocessing.Pool(NUM_CPUS) as p:
-                results_test = p.map(run_test_wrapper, args_list_for_test)
+        for row in test_results:
+            csvwriter.writerow(row)
 
-            for row in results_test:
-                csvwriter.writerow(row)
+
+if __name__ == "__main__":
+    main()
