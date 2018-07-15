d992fdc5d44c21caf03aec27f96ca5f65c8a1422
Zhuyi Xue <alfred532008@gmail.com>
2018-07-15 10:10:14 -0700

    updated/improved test_calc_genome_offset_with_clips.py

--- tests/test_misc/test_apautils/test_calc_genome_offset_with_clips.py
+++ tests/test_misc/test_apautils/test_calc_genome_offset_with_clips.py
@@ -4,38 +4,38 @@ from kleat.misc.apautils import calc_genome_offset
 import kleat.misc.settings as S
 
 
-@pytest.mark.parametrize("ctg_offset, expected_gnm_offset", [
+@pytest.mark.parametrize("ctg_clv, expected_gnm_offset", [
     [3, 0],                   # overlap with clv extracted from suffix evidence
     [4, 1],                   # bridge tail is a bit after the contig tail
 ])
-def test_for_contig_with_softclip(ctg_offset, expected_gnm_offset):
+def test_for_contig_with_softclip(ctg_clv, expected_gnm_offset):
     """
     TTT
     012       <-contig offset coord for tail
       └XXX    <-contig
        345    <-contig offset coord
+       ^ctg_clv
        012    <-genome offset coord
-
-    see parameters in the decorator for various ctg_offset
+       ^gnm_offset
     """
     ctg_cigartuples = ((S.BAM_CSOFT_CLIP, 3), (S.BAM_CMATCH, 3))
-    assert calc_genome_offset(ctg_cigartuples, ctg_offset, 'left') == expected_gnm_offset
+    assert calc_genome_offset(ctg_cigartuples, ctg_clv, 'left') == expected_gnm_offset
 
 
-@pytest.mark.parametrize("ctg_offset, expected_gnm_offset", [
-    [2, 0],                   # overlap with clv extracted from suffix evidence
-    [3, 1],                   # bridge tail is a bit after the contig tail
+@pytest.mark.parametrize("ctg_clv, expected_gnm_offset", [
+    [2, 0],
+    [3, 1],
+    [4, 2],
 ])
-def test_for_contig_with_hardclip(ctg_offset, expected_gnm_offset):
+def test_for_contig_with_hardclip(ctg_clv, expected_gnm_offset):
     """
-    The calculation with soft-clipped contig is the same except the CIGAR
-
-     TT
-     01       <-contig offset coord for tail
-      └XXX    <-contig
-       234    <-contig offset coord
-       012    <-genome offset coord
-
+    TT
+     └ACG
+    \\ACGT     <-contig
+    0123456    <-contig offset coord
+      ^ctg_clv
+      0123     <-genome offset coord
+      ^gnm_offset
     """
     ctg_cigartuples = ((S.BAM_CHARD_CLIP, 2), (S.BAM_CMATCH, 3))
-    assert calc_genome_offset(ctg_cigartuples, ctg_offset, 'left') == expected_gnm_offset
+    assert calc_genome_offset(ctg_cigartuples, ctg_clv, 'left') == expected_gnm_offset
