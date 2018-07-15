bc04b8661da23beb7fff49ccaa1f5620f9de0314
Zhuyi Xue <alfred532008@gmail.com>
2018-07-15 10:12:16 -0700

    added a commented test block for test_for_contig_with_clv_in_hardclip

--- tests/test_misc/test_apautils/test_calc_genome_offset_with_clips.py
+++ tests/test_misc/test_apautils/test_calc_genome_offset_with_clips.py
@@ -39,3 +39,25 @@ def test_for_contig_with_hardclip(ctg_clv, expected_gnm_offset):
     """
     ctg_cigartuples = ((S.BAM_CHARD_CLIP, 2), (S.BAM_CMATCH, 3))
     assert calc_genome_offset(ctg_cigartuples, ctg_clv, 'left') == expected_gnm_offset
+
+
+# In such case, there won't be a gnm_offset, not sure if would ever happen yet,
+# think about it later
+
+# @pytest.mark.parametrize("ctg_clv, expected_gnm_offset", [
+#     [2, 0],
+#     [3, 1],
+#     [4, 2],
+# ])
+# def test_for_contig_with_clv_in_hardclip(ctg_clv, expected_gnm_offset):
+#     """
+#     TT
+#      └ACG
+#     \\\CGT     <-contig
+#     0123456    <-contig offset coord
+#       ^ctg_clv
+#       0123     <-genome offset coord
+#       ^gnm_offset
+#     """
+#     ctg_cigartuples = ((S.BAM_CHARD_CLIP, 2), (S.BAM_CMATCH, 3))
+#     assert calc_genome_offset(ctg_cigartuples, ctg_clv, 'left') == expected_gnm_offset
