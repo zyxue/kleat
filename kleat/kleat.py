876a43626d39804d5c579a7330d4bf657218a9e0
Zhuyi Xue <alfred532008@gmail.com>
2018-07-23 10:21:05 -0700

    factored out kleat/polya.py

--- kleat/kleat.py
+++ kleat/kleat.py
@@ -2,16 +2,12 @@
 
 import os
 import logging
-import csv
 import multiprocessing
 
-import pysam
-from tqdm import tqdm
 import pandas as pd
 
-from kleat.misc import apautils
+from kleat import polya
 from kleat.args import get_args
-from kleat.proc import process_suffix, process_bridge_and_link, process_blank
 from kleat.post import (
     cluster_clv_parallel,
     aggregate_polya_evidence,
@@ -22,76 +18,10 @@ from kleat.post import (
 from kleat.misc import utils as U
 from kleat.misc import settings as S
 
-
 logging.basicConfig(
     level=logging.DEBUG, format='%(asctime)s|%(levelname)s|%(message)s')
 
 logger = logging.getLogger(__name__)
-
-
-def prepare_args_for_collect_polya_evidence(num_cpus, output, c2g_bam_file, *args):
-    c2g_bam = pysam.AlignmentFile(c2g_bam_file)
-
-    args_list = []
-    for k, seqname in tqdm(enumerate(c2g_bam.references),
-                           desc='prepared', unit=' seqnames'):
-
-        tmp_output_file = gen_tmp_output(output)
-        tmp_output_file += '.{0}'.format(seqname)
-        U.backup_file(tmp_output_file)
-
-        the_args = (seqname, tmp_output_file, c2g_bam_file) + args
-        args_list.append(the_args)
-    return args_list
-
-
-def collect_polya_evidence(seqname, tmp_output_file, c2g_bam_file,
-                           r2c_bam_file, ref_fa_file, bridge_skip_check_size):
-    """loop through each contig and collect polyA evidence"""
-    logging.info('collecting polyA evidence for {0} ...'.format(seqname))
-
-    c2g_bam = pysam.AlignmentFile(c2g_bam_file)
-    r2c_bam = pysam.AlignmentFile(r2c_bam_file)
-    ref_fa = pysam.FastaFile(ref_fa_file)
-
-    with open(tmp_output_file, 'wt') as opf:
-        csvwriter = csv.writer(opf, delimiter='\t')
-        csvwriter.writerow(S.HEADER)
-        for contig in c2g_bam.fetch(seqname):
-            if contig.is_unmapped:
-                continue
-
-            do_collection(contig, r2c_bam, ref_fa, csvwriter, bridge_skip_check_size)
-
-    return tmp_output_file
-
-
-def do_collection(contig, r2c_bam, ref_fa, csvwriter, bridge_skip_check_size):
-    gen_key = apautils.gen_clv_key_tuple_from_clv_record
-
-    ascs = []           # already supported clvs
-    rec = process_suffix(
-        contig, r2c_bam, ref_fa, csvwriter)
-    if rec is not None:
-        ascs.append(gen_key(rec))
-
-    for rec in process_bridge_and_link(
-            contig, r2c_bam, ref_fa, csvwriter, bridge_skip_check_size):
-        # TODO: with either bridge or link, they probably won't support
-        # clv of the other strand
-        ascs.append(gen_key(rec))
-
-    if not apautils.has_tail(contig):
-        process_blank(contig, ref_fa, csvwriter, ascs)
-
-
-def collect_polya_evidence_wrapper(args):
-    return collect_polya_evidence(*args)
-
-
-def gen_tmp_output(output):
-    return os.path.join(
-        os.path.dirname(output), '__tmp_{0}.tsv'.format(os.path.basename(output)))
 
 
 def gen_output(args_output, output_format):
@@ -125,14 +55,14 @@ def main():
     output = gen_output(args.output, args.output_format.lower())
     U.backup_file(output)
 
-    args_list = prepare_args_for_collect_polya_evidence(
+    args_list = polya.prepare_args_for_collect_polya_evidence(
         args.num_cpus, output, c2g_bam_file,
         r2c_bam_file, ref_fa_file, args.bridge_skip_check_size
     )
 
     logger.info('Processing contigs in parallel with {0} CPUs...'.format(args.num_cpus))
     with multiprocessing.Pool(args.num_cpus) as p:
-        tmp_tsv_files = p.map(collect_polya_evidence_wrapper, args_list)
+        tmp_tsv_files = p.map(polya.collect_polya_evidence_wrapper, args_list)
 
     logger.info('Reading {0} files into a single pandas.DataFrame...'.format(len(tmp_tsv_files)))
     dfs = []
@@ -149,7 +79,7 @@ def main():
         os.remove(f)
 
     if args.keep_pre_aggregation_tmp_file:
-        tmp_output = gen_tmp_output(output)
+        tmp_output = polya.gen_tmp_output(output)
         U.backup_file(tmp_output)
         logger.info('Dumping raw results before aggregation to {0}'.format(tmp_output))
         df_clv.to_csv(tmp_output, sep='\t', index=False)
---
+++ kleat/polya.py
@@ -0,0 +1,78 @@
+import os
+import csv
+import logging
+
+import pysam
+from tqdm import tqdm
+
+from kleat.misc import apautils
+from kleat.proc import process_suffix, process_bridge_and_link, process_blank
+from kleat.misc import utils as U
+from kleat.misc import settings as S
+
+logger = logging.getLogger(__name__)
+
+
+def gen_tmp_output(output):
+    return os.path.join(
+        os.path.dirname(output), '__tmp_{0}.tsv'.format(os.path.basename(output)))
+
+
+def prepare_args_for_collect_polya_evidence(num_cpus, output, c2g_bam_file, *args):
+    c2g_bam = pysam.AlignmentFile(c2g_bam_file)
+
+    args_list = []
+    for k, seqname in tqdm(enumerate(c2g_bam.references),
+                           desc='prepared', unit=' seqnames'):
+
+        tmp_output_file = gen_tmp_output(output)
+        tmp_output_file += '.{0}'.format(seqname)
+        U.backup_file(tmp_output_file)
+
+        the_args = (seqname, tmp_output_file, c2g_bam_file) + args
+        args_list.append(the_args)
+    return args_list
+
+
+def collect_polya_evidence(seqname, tmp_output_file, c2g_bam_file,
+                           r2c_bam_file, ref_fa_file, bridge_skip_check_size):
+    """loop through each contig and collect polyA evidence"""
+    logging.info('collecting polyA evidence for {0} ...'.format(seqname))
+
+    c2g_bam = pysam.AlignmentFile(c2g_bam_file)
+    r2c_bam = pysam.AlignmentFile(r2c_bam_file)
+    ref_fa = pysam.FastaFile(ref_fa_file)
+
+    with open(tmp_output_file, 'wt') as opf:
+        csvwriter = csv.writer(opf, delimiter='\t')
+        csvwriter.writerow(S.HEADER)
+        for contig in c2g_bam.fetch(seqname):
+            if contig.is_unmapped:
+                continue
+
+            do_collection(contig, r2c_bam, ref_fa, csvwriter, bridge_skip_check_size)
+
+    return tmp_output_file
+
+
+def do_collection(contig, r2c_bam, ref_fa, csvwriter, bridge_skip_check_size):
+    gen_key = apautils.gen_clv_key_tuple_from_clv_record
+
+    ascs = []           # already supported clvs
+    rec = process_suffix(
+        contig, r2c_bam, ref_fa, csvwriter)
+    if rec is not None:
+        ascs.append(gen_key(rec))
+
+    for rec in process_bridge_and_link(
+            contig, r2c_bam, ref_fa, csvwriter, bridge_skip_check_size):
+        # TODO: with either bridge or link, they probably won't support
+        # clv of the other strand
+        ascs.append(gen_key(rec))
+
+    if not apautils.has_tail(contig):
+        process_blank(contig, ref_fa, csvwriter, ascs)
+
+
+def collect_polya_evidence_wrapper(args):
+    return collect_polya_evidence(*args)
