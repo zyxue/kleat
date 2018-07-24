39548546ffd64355fdf3bed214f811e54717f3ae
Zhuyi Xue <alfred532008@gmail.com>
2018-07-24 09:09:26 -0700

    revamped is_a_suffix_read due to previous wrong understanding #18

--- kleat/evidence/suffix.py
+++ kleat/evidence/suffix.py
@@ -39,23 +39,35 @@ def calc_strand(tail_side):
                          'but {0} passed'.format(tail_side))
 
 
-def is_a_suffix_read(read, contig, ctg_tail_len=None):
+def is_a_suffix_read(read, contig, ctg_clv):
     """
     :param read: read
     :param contig: a suffix contig
     :param tail_len: the tail length of the suffix_contig
     """
-    if ctg_tail_len is None:
-        ctg_tail_len = apautils.calc_tail_length(contig)
-    if apautils.left_tail(read):
-        return read.reference_start == ctg_tail_len
-    elif apautils.right_tail(read):
-        return read.reference_end == contig.infer_query_length(always=True) - 1 - ctg_tail_len
+    if not contig.is_reverse:
+        if apautils.left_tail(contig):
+            offset = read.reference_start
+            return set(read.query_sequence[0:ctg_clv - offset]) == {'T'}
+        elif apautils.right_tail(contig):
+            delta = read.reference_end - ctg_clv
+            return set(read.query_sequence[-delta:]) == {'A'}
+        else:
+            return False
     else:
-        return False
+        n_ctg_clv = contig.infer_query_length(always=True) - ctg_clv - 1
+        if apautils.left_tail(contig):
+            delta = read.reference_end - n_ctg_clv
+            return set(read.query_sequence[-delta:]) == {'A'}
+        elif apautils.right_tail(contig):
+
+            offset = read.reference_start
+            return set(read.query_sequence[0:n_ctg_clv - offset]) == {'T'}
+        else:
+            return False
 
 
-def analyze_suffix_reads(r2c_bam, contig, ctg_clv, ctg_tail_len):
+def analyze_suffix_reads(r2c_bam, contig, ctg_clv):
     """
     :param contig: suffix contig
     """
@@ -63,13 +75,13 @@ def analyze_suffix_reads(r2c_bam, contig, ctg_clv, ctg_tail_len):
     if not contig.is_reverse:
         reads = r2c_bam.fetch(contig.query_name, ctg_clv, ctg_clv + 1)
     else:
-        rev_ctg_clv = contig.infer_query_length(always=True) - ctg_clv
+        rev_ctg_clv = contig.infer_query_length(always=True) - ctg_clv - 1
         reads = r2c_bam.fetch(contig.query_name, rev_ctg_clv, rev_ctg_clv + 1)
 
     for read in reads:
         if read.is_unmapped:
             continue
-        if is_a_suffix_read(read, contig, ctg_tail_len):
+        if is_a_suffix_read(read, contig, ctg_clv):
             max_tail_len = max(max_tail_len, apautils.calc_tail_length(read))
             num_suffix_reads += 1
     return num_suffix_reads, max_tail_len
@@ -94,7 +106,7 @@ def gen_clv_record(contig, r2c_bam, tail_side, ref_fa):
         ctg_clv = ctg_seq_len - ctg_tail_len - 1
 
     num_suffix_reads, max_suffix_read_tail_len = analyze_suffix_reads(
-        r2c_bam, contig, ctg_clv, ctg_tail_len)
+        r2c_bam, contig, ctg_clv)
 
     ctg_hex, ctg_hex_id, ctg_hex_pos = gen_contig_hexamer_tuple(
         contig, strand, ref_clv, ref_fa, ctg_clv)
